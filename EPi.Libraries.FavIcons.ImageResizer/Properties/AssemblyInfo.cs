b5198bd37c799c1f604dc1228500597a43a9a287
jeroen.stemerdink <jeroen.stemerdink@efocus.nl>
2016-03-14 16:58:00 +0100

    Added GitVersionTask

--- EPi.Libraries.FavIcons.ImageResizer/Properties/AssemblyInfo.cs
+++ EPi.Libraries.FavIcons.ImageResizer/Properties/AssemblyInfo.cs
@@ -48,17 +48,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("e10b62e4-cb90-4520-8b38-4b9426538bf7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
